b137d92c955d57d0f5bae941fbeee4852d04c0a7
Krettsy <hdhduudidi@gmail.com>
2024-10-18 20:22:45 +0300

    chore: update api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "/" : "194.87.26.15:8080",
+  baseURL:
+    import.meta.env.MODE === "development" ? "/" : "http://194.87.26.15:8080",
 });
